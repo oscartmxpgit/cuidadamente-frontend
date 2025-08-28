b74afe8fb807592aad795032478cb55066efdfa5
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2025-08-28 17:47:08 +0200

    mejoras estilos

--- src/app/home/home-hero/home-hero.component.ts
+++ src/app/home/home-hero/home-hero.component.ts
@@ -1,9 +1,12 @@
+import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { Component } from '@angular/core';
 import { TRANSITION_TEXT, TRANSITION_TEXT_ENTER, ENTER_SCALE } from './transitions.constants';
+import { FileService } from '../../tarjetas/fileService';
 
 @Component({
   selector: 'app-home-hero',
+  templateUrl: './home-hero.component.html',
+  styleUrl: './home-hero.component.scss',
   animations: [
     trigger('cardHover', [
       state('hover', style({ transform: 'scale(1.05)' })),
@@ -20,11 +23,20 @@ import { TRANSITION_TEXT, TRANSITION_TEXT_ENTER, ENTER_SCALE } from './transitio
     TRANSITION_TEXT,
     TRANSITION_TEXT_ENTER,
     ENTER_SCALE
-  ],
-  templateUrl: './home-hero.component.html',
-  styleUrl: './home-hero.component.scss'
+  ]
 })
-export class HomeHeroComponent {
+export class HomeHeroComponent implements OnInit {
+  imageUrl: string = '';
+
+  constructor(private fileService: FileService) {}
+
+  ngOnInit() {
+    // Load your background image from service
+    this.fileService.loadImage('FotoPrincipal.jpg').subscribe(url => {
+      this.imageUrl = url;
+    });
+  }
+
   scrollToContact() {
     const element = document.getElementById('contact');
     if (element) {
